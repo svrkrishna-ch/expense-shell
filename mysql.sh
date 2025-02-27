9a74164396679b12836a0524af86eb6bc522ed8c
svrkrishna-ch <ramakrishna.chilla074@gmail.com>
2025-02-27 21:29:57 +0530

    expense shell

---
+++ mysql.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/expense-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1 )
+TIMESTAMP=$(date +%y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2 - $R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2 - $G SUCCESS $N"
+    fi
+    
+}
+
+CHECK_ROOT(){
+    if [ $1 -ne 0 ]
+    then 
+        echo "ERROR:: You must have sudo access to execute this script"
+        exit 1
+
+}
+
+echo "Script started executing at $TIMESTAMP" &>>$LOG_FILE_NAME
+
+CHECK_ROOT
+
+dnf install mysql server -y &>>$LOG_FILE_NAME
+VALIDATE $? "Installing MySQL server"
+
+systemsctl enable mysqld &>>$LOG_FILE_NAME
+VALIDATE $? "Enabling MySQL server"
+
+systemsctl start mysqld &>>$LOG_FILE_NAME
+VALIDATE $? "Starting MySQL server"
+
+mysql_secure_installation --set-root-pass ExpenseApp@1
+VALIDATE $? "Setting Root Password"
+
