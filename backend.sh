903a6f04ede24bfd26c09ac6cff296ead12409fc
svrkrishna-ch <ramakrishna.chilla074@gmail.com>
2025-02-27 23:10:40 +0530

    expense shell

--- backend.sh
+++ backend.sh
@@ -52,7 +52,7 @@ else
 fi
 
 
-mkdir /app &>>$LOG_FILE_NAME
+mkdir -p /app &>>$LOG_FILE_NAME
 VALIDATE $? "Creating App Directory"
 
 curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE_NAME
