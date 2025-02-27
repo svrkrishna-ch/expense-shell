7751036ccf679b3ab0214eae86b6449e8263f443
svrkrishna-ch <ramakrishna.chilla074@gmail.com>
2025-02-27 23:02:42 +0530

    expense shell

--- backendsample.sh
+++ backendsample.sh
@@ -34,3 +34,4 @@ echo "Script started executing at $TIMESTAMP" &>>$LOG_FILE_NAME
 CHECK_ROOT
 
 cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service
+VALIDATE $? "Backend Copied"
